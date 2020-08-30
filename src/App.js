0b0c4d1b83f45ce86c2162fcc4b23ce5db185d3d
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-29 19:21:49 -0500

    create app component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+//import react
+import React from 'react'
+
+//create and export app functional component
+export const App = () => {
+  return(
+    <>
+      <p>I'm the app!</p>
+    </>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+//import react
+import React from 'react';
+//import render method
+import { render } from 'react-dom';
+//import App component
+import App from './App'
+
+//create root const
+const root = document.getElementById('root');
+
+//render the app
+render(<App />, root);
